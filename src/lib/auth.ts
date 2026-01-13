e2c8291152c074aeef86eca69a0cdb5841d8de16
shishir <mdabdulaziz3745@gmail.com>
2026-01-13 16:34:04 +0600

    initialized learnNest web app with next.js, shadCn, postgres with prisma and better auth

---
+++ src/lib/auth.ts
@@ -0,0 +1,10 @@
+import { Prisma } from "@/generated/prisma/client";
+import { betterAuth } from "better-auth";
+import { prismaAdapter } from "better-auth/adapters/prisma";
+// If your Prisma file is located elsewhere, you can change the path
+
+export const auth = betterAuth({
+  database: prismaAdapter(Prisma, {
+    provider: "postgresql",
+  }),
+});
